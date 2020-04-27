f029d508b307ce6a5280fdfcae78d4306b53f1d9
Manesha Ramesh <41325932+ManeshaRamesh@users.noreply.github.com>
2020-04-28 02:57:05 +0400

    Update python-script.py

--- python-server/python-script.py
+++ python-server/python-script.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -6,6 +5,14 @@ import cgi
 import cgitb
 import json
 import random
+import pymysql.cursors
+from os import path, listdir
+from tempfile import gettempdir
+import os
+import binascii
+import time
+
+
 
 import requests
 from collections import OrderedDict
@@ -35,6 +42,12 @@ print ""
 # PROXYDICT = {"http": HTTP_PROXY, "https": HTTPS_PROXY}
 # PROXY = PROXY_IP + ":" + str(PROXY_PORT)
 # ENCODING = "utf-8"
+connection = pymysql.connect(host='86.97.179.52',
+                             user='jscleaner_remote',
+                             port=8306,
+                             password='comnets@2019',
+                             db='mydb_mitmjscleaner',
+                             autocommit=True)
 
 
 filename = "ml_model.sav"
@@ -106,7 +119,7 @@ FEATURES = ['replace',
 
 def get_resource(url):
     """Request resource from proxy and return response."""
-    req = requests.get(url)
+    req = requests.get(url, verify=False)
     try:
         return req.text
     except KeyError:
@@ -146,40 +159,93 @@ cgitb.enable()
 form = cgi.FieldStorage()
 data = []
 
-try:
-	urls = form["url"].value.split(",")
+entry = []
 
-	for url in urls:
+toBeLabelled = [] #scripts not in teh database
+
+
+# try:
+urls = form["url"].value.split(",")
+
+
+# find already labelled scripts add to data list
+for url in urls:
+	with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+	# print str(item)
+		query = "SELECT * FROM pluginLabels where requestUrl = '"+url + "'"
+		cursor.execute(query)
+		result = cursor.fetchone()
+		# print result
+		if result is None:
+			toBeLabelled.append(url)
+		else:
+			# json_string = "{" + "name: '"+ result["requestUrl"] + "', label: '" + result["label"] + "', accuracy: '" + result["accuracy"] + "'}"
+			# if result != None:
+			# toBeLabelled.remove(url)
+			if result["label"] == 'cdn':
+				result["label"] = 'CDN'
+			elif result["label"] == 'ads':
+				result["label"] = 'Advertising'
+			elif result["label"] == 'tag-manager':
+				result["label"] = 'Tag Manager'
+			elif result["label"] == 'customer-success':
+				result["label"] = 'Customer Success'
+			else:
+				result["label"] = result["label"].capitalize()
+
+
+			data.append({"name": result["requestUrl"], "label": result["label"], "accuracy": result["accuracy"]})
+
+			
+
+cursor.close()
+
+
+labelledscripts = []
+# print json.dumps(data, indent=4)
+
+# label unlabelled scripts and add to data list
+
+try:
+	for url in toBeLabelled:
 		content = get_resource(url);
 		vector = extract_features(content);
 		new_output = loaded_model.predict([vector]) #this is your label to be displayed (ex: Ads...) c
 		confidence = np.amax(loaded_model.predict_proba([vector])) #this is the confidence of prediction
 	    # print (new_output , confidence)
 	    # json_object =  json_object + "{ 'name': '"+url +"', 'label': '" + new_output[0] + "',  'accuracy': '"+  str(confidence) +"'} ,"
-
+		entry.append(url)
+		entry.append(new_output[0])
+		entry.append(confidence)
+		labelledscripts.append(entry)
 		if new_output[0] == 'cdn':
 			new_output[0] = 'CDN'
 		elif new_output[0] == 'ads':
 			new_output[0] = 'Advertising'
-		elif new_output[0] == 'Tag-manager':
+		elif new_output[0] == 'tag-manager':
 			new_output[0] = 'Tag Manager'
-		elif new_output[0] == 'Tag-manager':
-			new_output[0] = 'Tag Manager'
+		elif new_output[0] == 'customer-success':
+			new_output[0] = 'Customer Success'
 		else:
 			new_output[0] = new_output[0].capitalize()
 
 		data.append({"name":url, "label":new_output[0], "accuracy":confidence})
 
 	print json.dumps(data, indent=4)
-
 except:
 	print "Error"
 
+try: 
+	for item in labelledscripts:
+			with connection.cursor() as cursor:
+				# print str(item)
+				query = "INSERT INTO pluginLabels (requestUrl, label, accuracy) VALUES (%s, %s, %s)"
+				cursor.execute(query, (item[0], item[1], str(round(float(item[2]), 2)))
+)
+finally: 
+	cursor.close()
 
 
 
-
-
-
-
-
+# except:
+# 	print "Error: "
